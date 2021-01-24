92643e4df0f37e1ed99496948eaa878e8e81f9cb
Matias Duhalde <matias.duhalde@uc.cl>
2021-01-24 21:38:14 +0000

    :sparkles: Add approval card component and test children property nesting feature

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="9:00 AM" content={faker.lorem.sentence()} avatar={faker.image.image()} />
-            <CommentDetail author="Moe" timeAgo="3:33 AM" content={faker.lorem.sentence()} avatar={faker.image.image()} />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="9:00 AM"
+                    content={faker.lorem.sentence()}
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Moe"
+                    timeAgo="3:33 AM"
+                    content={faker.lorem.sentence()}
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
         </div>
     );
 }
