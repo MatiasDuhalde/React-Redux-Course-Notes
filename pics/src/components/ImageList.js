6a51564d56bad4f3b105619ebcbff977eb9b9ea2
Matias Duhalde <matias.duhalde@uc.cl>
2021-01-30 05:06:20 +0000

    :lipstick: Render all images in a grid layout

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 class ImageList extends React.Component {
     render() {
-        const images = this.props.images.map(({ description, id, urls }) => {
-            return <img alt={description} key={id} src={urls.regular} />;
+        const images = this.props.images.map(image => {
+            return <ImageCard key={image.id} image={image} />;
         });
-        return <div>{images}</div>;
+        return <div className="image-list">{images}</div>;
     }
 }
 
