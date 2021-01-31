ae40fecf0b21c54a096139a392933957928713e0
Matias Duhalde <matias.duhalde@uc.cl>
2021-01-31 02:21:52 +0000

    :sparkles: Define callback function for youtube API request

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 
 class App extends React.Component {
+    state = { videos: [] };
+
+    onTermSubmit = async term => {
+        if (!term) {
+            this.setState({ videos: [] });
+            return;
+        }
+        const response = await youtube.get('/search', {
+            params: {
+                q: term,
+            },
+        });
+
+        this.setState({ videos: response.data.items });
+    };
+
     render() {
         return (
-            <div class="ui container">
-                <SearchBar />
+            <div className="ui container">
+                <SearchBar onSubmit={this.onTermSubmit} />
+                Found {this.state.videos.length} videos
             </div>
         );
     }
