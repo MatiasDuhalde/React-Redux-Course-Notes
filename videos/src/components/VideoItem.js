533ea6f28054507a6ac65abf22a54a3c65d3a690
Matias Duhalde <matias.duhalde@uc.cl>
2021-01-31 01:40:03 +0000

    :sparkles: Add initial scaffold for project structure

---
+++ videos/src/components/VideoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class VideoItem extends React.Component {
+    render() {
+        return (
+            <div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default VideoItem;
