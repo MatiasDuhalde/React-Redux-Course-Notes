00934de4b3dc8c77d734c59ecadd376dc6eeb032
Matias Duhalde <matias.duhalde@uc.cl>
2021-02-01 02:49:35 +0000

    :lipstick: Set components in a grid layout, and add default query

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,6 +7,10 @@ import youtube from '../api/youtube';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('JavaScript');
+    }
+
     onTermSubmit = async term => {
         if (!term) {
             this.setState({ videos: [] });
@@ -18,7 +22,10 @@ class App extends React.Component {
             },
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0],
+        });
     };
 
     onVideoSelect = video => {
@@ -29,11 +36,19 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    videos={this.state.videos}
-                    onVideoSelect={this.onVideoSelect}
-                />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                videos={this.state.videos}
+                                onVideoSelect={this.onVideoSelect}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
