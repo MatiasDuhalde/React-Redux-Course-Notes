e2f975c3be599dbc29f60b3b01e63bdc9c38a672
Matias Duhalde <matias.duhalde@uc.cl>
2021-02-01 04:29:00 +0000

    :lipstick: Add Semantic UI library and basic styling for accordion component

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework',
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers',
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components',
+    },
+];
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <div></div>
+                <Accordion items={items} />
             </div>
         );
     }
