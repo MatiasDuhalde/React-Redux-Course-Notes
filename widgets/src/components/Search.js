3d7d3dd3a2afad41bd72c8c04f55da7527d1a7a2
Matias Duhalde <matias.duhalde@uc.cl>
2021-02-07 21:11:05 +0000

    :bug: Refactor Search component to remove warnings

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 500);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -15,18 +26,17 @@ const Search = () => {
                         list: 'search',
                         origin: '*',
                         format: 'json',
-                        srsearch: term,
+                        srsearch: debouncedTerm,
                     },
                 }
             );
 
             setResults(data.query.search);
         };
-
-        if (term) {
+        if (debouncedTerm) {
             search();
         }
-    }, [term]);
+    }, [debouncedTerm]);
 
     const renderedResults = results.map(result => {
         return (
