724445e25026b24de8452ac6cd7175b2e88f9991
Matias Duhalde <matias.duhalde@uc.cl>
2021-02-09 00:34:13 +0000

    :recycle: Refactor simple components to use a function based structure

--- videos-hooks/src/components/VideoList.js
+++ videos-hooks/src/components/VideoList.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-class VideoList extends React.Component {
-    render() {
-        const renderedList = this.props.videos.map(video => {
-            return (
-                <VideoItem
-                    key={video.id.videoId}
-                    video={video}
-                    onVideoSelect={this.props.onVideoSelect}
-                />
-            );
-        });
-        return <div className="ui relaxed divided list">{renderedList}</div>;
-    }
-}
+const VideoList = ({ videos, onVideoSelect }) => {
+    const renderedList = videos.map(video => {
+        return (
+            <VideoItem
+                key={video.id.videoId}
+                video={video}
+                onVideoSelect={onVideoSelect}
+            />
+        );
+    });
+    return <div className="ui relaxed divided list">{renderedList}</div>;
+};
 
 export default VideoList;
