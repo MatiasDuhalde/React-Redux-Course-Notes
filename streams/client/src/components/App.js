2fcc0be38837489bf0c4cabe9dacbf405e2d27de
Matias Duhalde <matias.duhalde@uc.cl>
2021-02-14 18:56:07 +0000

    :sparkles: Add header component and reorganize routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -9,20 +9,14 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
-        <div>
-            <Header />
+        <div className="ui container">
             <BrowserRouter>
-                <div>
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route
-                        path="/streams/delete"
-                        exact
-                        component={StreamDelete}
-                    />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </div>
+                <Header />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+                <Route path="/streams/show" exact component={StreamShow} />
             </BrowserRouter>
         </div>
     );
