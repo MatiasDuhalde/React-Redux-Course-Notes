84f349bcec4136998699bbee86900a25aef6af56
Matias Duhalde <matias.duhalde@uc.cl>
2021-03-04 11:42:08 +0000

    :sparkles: Add basic sign in and sign out functionality

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -4,17 +4,63 @@ const CLIENT_ID =
     '477165084783-p36s62gb1uu15064vluileq8l9ftf37n.apps.googleusercontent.com';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
-                clientId: CLIENT_ID,
-                scope: 'email',
-            });
+            window.gapi.client
+                .init({
+                    clientId: CLIENT_ID,
+                    scope: 'email',
+                })
+                .then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance();
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                    this.auth.isSignedIn.listen(this.onAuthChange);
+                });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button
+                    onClick={this.onSignOutClick}
+                    className="ui red google button"
+                >
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button
+                    onClick={this.onSignInClick}
+                    className="ui green google button"
+                >
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
