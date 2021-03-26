d5ff0df5be8d40ecc46342d60868de20c27c84c5
Matias Duhalde <matias.duhalde@uc.cl>
2021-03-26 17:12:48 -0300

    :sparkles: Add reducer for managing streams actions

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import streamsReducer from './streamsReducer';
+
+// React-final-form must be wired
 
 export default combineReducers({
     auth: authReducer,
+    streams: streamsReducer,
 });
