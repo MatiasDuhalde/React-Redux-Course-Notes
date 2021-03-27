18b2c0736df4e8373a614fd0d5cf82d9605ec1de
Matias Duhalde <matias.duhalde@uc.cl>
2021-03-26 21:47:02 -0300

    :recycle: Refactor forms to recycle code, edit stream now working

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,70 +1,19 @@
 import React from 'react';
-import { Field, Form } from 'react-final-form';
 import { connect } from 'react-redux';
 import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-    renderInput = ({ input, label, meta }) => {
-        const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-        return (
-            <div className={className}>
-                <label>{label}</label>
-                <input {...input} autoComplete="off" />
-                {(() => this.renderError(meta))()}
-            </div>
-        );
-    };
-
-    renderError({ error, touched }) {
-        if (error && touched) {
-            return (
-                <div className="ui error message">
-                    <div>{error}</div>
-                </div>
-            );
-        }
-        return null;
-    }
-
     onSubmit = formValues => {
         this.props.createStream(formValues);
     };
 
-    validate(formValues) {
-        const errors = {};
-        if (!formValues.title) {
-            errors.title = 'You must enter a title';
-        }
-        if (!formValues.description) {
-            errors.description = 'You must enter a description';
-        }
-        return errors;
-    }
-
     render() {
         return (
-            <Form
-                initialValues={{}}
-                onSubmit={this.onSubmit}
-                validate={this.validate}
-                render={({ handleSubmit, pristine, form, submitting }) => (
-                    <form className="ui form error" onSubmit={handleSubmit}>
-                        <Field
-                            name="title"
-                            label="Enter Title"
-                            component={this.renderInput}
-                        />
-                        <Field
-                            name="description"
-                            label="Enter Description"
-                            component={this.renderInput}
-                        />
-                        <button className="ui button primary" type="submit">
-                            Submit
-                        </button>
-                    </form>
-                )}
-            ></Form>
+            <div>
+                <h3>Create a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} />
+            </div>
         );
     }
 }
--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = formValues => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
+
     render() {
         if (!this.props.stream) {
             return <div>Loading...</div>;
         }
-        return <div>StreamEdit</div>;
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    onSubmit={this.onSubmit}
+                    initialValues={{
+                        title: this.props.stream.title,
+                        description: this.props.stream.description,
+                    }}
+                />
+            </div>
+        );
     }
 }
 
