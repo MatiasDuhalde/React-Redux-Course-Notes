3e2eb1daae08e702991329b04632c6694d49a0dd
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-14 19:07:42 -0400

    :sparkles: Remove trivial context from Field

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
-  static contextType = LanguageContext;
-
   render() {
     return (
       <div className="ui field">
