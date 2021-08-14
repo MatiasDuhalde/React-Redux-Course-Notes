3578eb1e8108213904d0c7311ffd340b5343929a
Matias Duhalde <matias.duhalde@uc.cl>
2021-08-14 01:27:20 -0400

    :art: Remove comment

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -2,8 +2,6 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import streamsReducer from './streamsReducer';
 
-// React-final-form must be wired
-
 export default combineReducers({
   auth: authReducer,
   streams: streamsReducer,
